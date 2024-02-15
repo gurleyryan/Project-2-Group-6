8dfd50fe0fc5c301c60ed32146c80bd2e5f5440a
Ryan Gurley <gurleyryan@gmail.com>
2024-02-14 19:56:41 -0800

    Reorganized file structure

---
+++ festival.js
@@ -0,0 +1,25 @@
+// models/Festival.js
+
+const { Sequelize, DataTypes } = require('sequelize');
+const sequelize = require('./config/database');
+
+const Festival = sequelize.define('Festival', {
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  location: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  startDate: {
+    type: DataTypes.DATE,
+    allowNull: false
+  },
+  endDate: {
+    type: DataTypes.DATE,
+    allowNull: false
+  }
+});
+
+module.exports = Festival;
