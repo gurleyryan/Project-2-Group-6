07a13674c0f32fcee2a91f592d60435444a4611f
jmartincampos <jmartincampos18@gmail.com>
2024-02-16 16:28:58 -0800

    commit changes to RESTfulAPI and package.json

--- FestivalHub/utils/auth.js
+++ FestivalHub/utils/auth.js
@@ -1,1 +1,26 @@
 // enter authonrization code below
+const authenticateUser = (req, res, next) => {
+    //code to verify token and authen user
+    const token = req.header.authorization;
+    if (!toekn || !isValidToken (token)){
+        return res.status(401).json({error: "unauthorized"});
+    }
+next ();
+};
+
+//middleware function to check user permissions
+const checkUserPermissions = (req, res, next) => {
+    const userId = re.user.userId;
+    const resourceID = req.params. resourceId;
+
+    //if user does not have permission to access the source
+    if (!hasPermission(userId, resourceId)) {
+        return res.status(403).json({error: 'forbidden'});
+    }
+    next();
+};
+
+module.exports ={
+    authenticateUser,
+    checkUserPermissions,
+};
--- FestivalHub/utils/helpers.js
+++ FestivalHub/utils/helpers.js
@@ -1,1 +1,19 @@
 // helpers.js
+const generateRandomString = (length) => {
+    let result= (any);
+    const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+    for (let i=0; i<length; i++) {
+        result += characters.charAt(Math.floor(Math.random*(any) * characters.length));
+    }
+    return result;
+};
+
+const isValidEmail = (email) => {
+    const emailRegex = /[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+};
+
+module.exports = {
+    generateRandomString,
+    isValidEmail,
+};
