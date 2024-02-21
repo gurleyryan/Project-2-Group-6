b0d0383431397420f01bab4da2bc024bd7386e49
Ryan Gurley <gurleyryan@gmail.com>
2024-02-20 23:01:39 -0500

    debugging LOTS

---
+++ FestivalHub/models/Booking.js
@@ -0,0 +1,47 @@
+// models/Booking.js
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Booking extends Model { }
+
+Booking.init(
+  {
+    booking_id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    number_of_tickets: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    booking_date: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+    },
+    ticket_price: {
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: false
+    }
+  },
+  {
+    hooks: {
+      // beforeCreate: async (newUserData) => {
+      //   newUserData.password = await bcrypt.hash(newUserData.password, 10);
+      //   return newUserData;
+      // },
+      // beforeUpdate: async (updatedUserData) => {
+      //   updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+      //   return updatedUserData;
+      // },
+    },
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'booking',
+  }
+);
+
+module.exports = Booking;
---
+++ FestivalHub/models/index.js
@@ -0,0 +1,8 @@
+const Booking = require('./Booking');
+const Festival = require('./Festival');
+const Performer = require('./Performer');
+const User = require('./User');
+
+
+
+module.exports = { Booking, Festival, Performer, User };
