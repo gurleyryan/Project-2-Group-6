90505891a7d180f5fa5700c3991544ab5b5bebc7
Ryan Gurley <48134032+gurleyryan@users.noreply.github.com>
2024-02-26 23:48:37 -0500

    Delete FestivalHub directory

--- FestivalHub/models/index.js
+++
@@ -1,8 +0,0 @@
-const Booking = require('./Booking');
-const Festival = require('./Festival');
-const Performer = require('./Performer');
-const User = require('./User');
-
-
-
-module.exports = { Booking, Festival, Performer, User };
